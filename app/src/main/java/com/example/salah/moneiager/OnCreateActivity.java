fb78646edc8b1908b1e616371e280d5dbd7b02bb
Salah Alkmali <mtnshsalah@gmail.com>
2019-01-05 17:23:55 +0300

    Styling The Application

---
+++ app/src/main/java/com/example/salah/moneiager/OnCreateActivity.java
@@ -0,0 +1,13 @@
+package com.example.salah.moneiager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_create);
+    }
+}
